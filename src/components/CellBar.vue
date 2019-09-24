e4581e89d49e1b55f913473acded5f9ed9387697
T-demon <52730601+T-demon@users.noreply.github.com>
2019-09-24 15:48:46 +0800

    条形组件

---
+++ src/components/CellBar.vue
@@ -0,0 +1,42 @@
+<!--
+ * @Describe: 
+ * @Author: Tang
+ * @Date: 2019-09-24 15:08:50
+ * @LastEditTime: 2019-09-24 15:40:40
+ -->
+<template>
+  <div class="Cell-wrap" @click="$emit('click',$event)">
+    <div class="cell">
+      <span>{{label}}</span>
+      <i>{{text}}</i>
+      <span class="iconfont iconjiantou1"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+    props:["label","text"]
+};
+</script>
+
+<style lang="less" scoped>
+.Cell-wrap {
+  padding-left: 10px;
+  .cell {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-right: 10px;
+    height: 48 / 360 * 100vw;
+    border-bottom: 1px #eee solid;
+    i {
+      flex: 1;
+      font-size:12px;
+      color: #b5b5b5;
+      text-align: right;
+    }
+  }
+}
+</style>
--- src/pages/Personal.vue
+++ src/pages/Personal.vue
@@ -2,58 +2,62 @@
  * @Describe: 
  * @Author: Tang
  * @Date: 2019-09-24 14:07:45
- * @LastEditTime: 2019-09-24 15:00:59
+ * @LastEditTime: 2019-09-24 15:40:00
  -->
 <template>
   <div>
-      <div class="profile">
-          <img src="../../static/男头像.png">
-          <div class="profile-center">
-              <div class="name">
-                    <span class="iconfont iconxingbienan"></span>
-                    火星网友
-              </div>
-              <div class="time">
-                  2019-10-1
-              </div>
-          </div>
-          <i class="iconfont iconjiantou1"></i>
-
+    <div class="profile">
+      <img src="../../static/男头像.png" />
+      <div class="profile-center">
+        <div class="name">
+          <span class="iconfont iconxingbienan"></span>
+          火星网友
+        </div>
+        <div class="time">2019-10-1</div>
       </div>
+      <i class="iconfont iconjiantou1"></i>
+    </div>
+     <CellBar label="我的关注"  text="关注的用户"></CellBar>
+     <CellBar label="我的跟帖"  text="跟帖/回复"></CellBar>
+     <CellBar label="我的关注"  text="文章/视频"></CellBar>
+     <CellBar label="设置"></CellBar>
+
+
   </div>
 </template>
 
 <script>
-export default {
+import CellBar from "@/components/CellBar";
 
-}
+export default {
+    components:{
+        CellBar
+    }
+};
 </script>
 
 <style lang="less" scoped>
-    .profile{
-        display: flex;
-        padding: 20px;
-        border-bottom:2px solid #b8b8b8;
-        justify-content: space-around;
-        align-items: center;
-       img{
-            width: 70 / 360 * 100vw;
-            height: 70 / 360 * 100vw;
-            border-radius: 50%;
-       }
-       .profile-center{
-           flex: 1;
-           margin-left: 10px;
-           span{
-               color: skyblue;
-           }
-           .time{
-               color: #b8b8b8;
-               font-size: 14px;
-           }
-       }
-       
+.profile {
+  display: flex;
+  padding: 20px;
+  border-bottom: 2px solid #b8b8b8;
+  justify-content: space-around;
+  align-items: center;
+  img {
+    width: 70 / 360 * 100vw;
+    height: 70 / 360 * 100vw;
+    border-radius: 50%;
+  }
+  .profile-center {
+    flex: 1;
+    margin-left: 10px;
+    span {
+      color: skyblue;
     }
-</style>>
-
+    .time {
+      color: #b8b8b8;
+      font-size: 14px;
+    }
+  }
+}
 </style>
