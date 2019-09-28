a1d9813ab9fc1d6b64015fa6baca1d1d56717f1b
T-demon <52730601+T-demon@users.noreply.github.com>
2019-09-28 16:09:42 +0800

    底部页面布局

---
+++ src/components/PostFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="footer">
+    <input type="text" placeholder="写跟帖" @focus="handleFocus" class="gt"/>
+
+    <span class="comment">
+      <em>1020</em>
+      <i class="iconfont iconpinglun-"></i>
+    </span>
+
+    <i class="iconfont iconshoucang"></i>
+
+    <i class="iconfont iconfenxiang"></i>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="less">
+.footer {
+    display: flex;
+    .gt{
+        width: 
+    }
+}
+</style>
+
--- src/pages/PostDetail.vue
+++ src/pages/PostDetail.vue
@@ -1,41 +1,53 @@
 <template>
-  <div class="detail">
-    <div class="detail-header">
-      <span class="iconfont iconjiantou2"></span>
-      <i class="iconfont iconnew"></i>
-      <span class="gzbtn">关注</span>
-    </div>
-    <h3>火星时报火星时报火星时报火星时报火星时时报</h3>
-    <div class="detail-info">
-      <span>火星时报</span>
-      <span>201-10-10</span>
-    </div>
-    <div class="detail-content">
-      火星时报火星时报火星时报火
-      星时报火星时报火星时星时报火星时
-      报火星时报火星时报火星时报火星时报
-      火星时星时报火星时报火星时报火星时报火星时报火星时报火星时星时报
-    </div>
-    <div class="post-btns">
+  <div>
+    <div class="detail">
+      <div class="detail-header">
+        <span class="iconfont iconjiantou2"></span>
+        <i class="iconfont iconnew"></i>
+        <span class="gzbtn">关注</span>
+      </div>
+      <h3>火星时报火星时报火星时报火星时报火星时时报</h3>
+      <div class="detail-info">
+        <span>火星时报</span>
+        <span>201-10-10</span>
+      </div>
+      <div class="detail-content">
+        火星时报火星时报火星时报火
+        星时报火星时报火星时星时报火星时
+        报火星时报火星时报火星时报火星时报
+        火星时星时报火星时报火星时报火星时报火星时报火星时报火星时星时报
+      </div>
+      <div class="post-btns">
         <span>
-            <i class="iconfont icondianzan"></i>
-            112
+          <i class="iconfont icondianzan"></i>
+          112
         </span>
         <span>
-            <i class="iconfont iconweixin"></i>
-            微信
+          <i class="iconfont iconweixin"></i>
+          微信
         </span>
+      </div>
+    </div>
+    <div class="foot">
+      <PostFooter></PostFooter>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import PostFooter from "@/components/PostFooter";
+export default {
+  components: {
+    PostFooter
+  }
+};
 </script>
 
 <style lang="less" scoped>
 .detail {
   padding: 0px 20px;
+  border-bottom: 3px solid #999;
+  padding-bottom: 20px;
   .detail-header {
     height: 60/360 * 100vw;
     display: flex;
@@ -62,22 +74,23 @@ export default {};
   .detail-content {
     margin-top: 10px;
     font-size: 14px;
+    line-height: 1.5;
   }
- 	.post-btns{
-    margin-top:30px;
+  .post-btns {
+    margin-top: 30px;
     display: flex;
     justify-content: space-around;
-    span{ 
-        padding: 0 15px;
-        height: 30 / 360 * 100vw;
-        line-height: 30 / 360 * 100vw;
-        font-size:12px;
-        border: 1px #ddd solid;
-        border-radius: 50px;
+    span {
+      padding: 0 15px;
+      height: 30 / 360 * 100vw;
+      line-height: 30 / 360 * 100vw;
+      font-size: 12px;
+      border: 1px #ddd solid;
+      border-radius: 50px;
     }
-    .iconweixin{
-        color: #07c907;
+    .iconweixin {
+      color: #07c907;
     }
-}
+  }
 }
 </style>
